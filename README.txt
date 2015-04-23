4eb0aedda1f3c06bd30f2a34c582bb80ac22c6eb
kunalnagar <kunalnagar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-23 03:47:52 +0000

    Publishing version: 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/custom-404-pro/trunk@1142823 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,34 @@
+=== Plugin Name ===
+Contributors: kunalnagar
+Donate link: http://kunalnagar.in/donate/
+Tags: 404
+Requires at least: 3.0.1
+Tested up to: 4.1.2
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Override the default 404 page with any page from the Admin Panel.
+
+== Description ==
+
+Allows users to replace the default 404 page with a custom page from the Pages section in the Admin Panel.
+
+Just select the desired page from the drop-down list and Save! And that's it!
+
+== Installation ==
+
+1. Extract the downloaded ZIP file.
+2. Copy the custom-404-pro folder to the wp-content/plugins directory.
+3. Activate from the Plugins Section.
+4. Options are available under: Settings > Custom 404 Pro
+
+== Screenshots ==
+
+1. Activate the plugin from the WordPress Admin Panel
+2. Go to Settings > Custom 404 Pro to open up the Options Page
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial Release
---
+++ custom-404-pro.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+Plugin Name: Custom 404 Pro
+Plugin URI: https://github.com/kunalnagar/custom-404-pro
+Description: Override the default 404 page with any page from the Admin Panel.
+Version: 1.0.1
+Author: Kunal Nagar
+Author URI: http://kunalnagar.in
+License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-custom-404-pro-activator.php
+ */
+function activate_custom_404_pro() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-custom-404-pro-activator.php';
+	Custom_404_Pro_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-custom-404-pro-deactivator.php
+ */
+function deactivate_custom_404_pro() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-custom-404-pro-deactivator.php';
+	Custom_404_Pro_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_custom_404_pro' );
+register_deactivation_hook( __FILE__, 'deactivate_custom_404_pro' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-custom-404-pro.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_custom_404_pro() {
+
+	$plugin = new Custom_404_Pro();
+	$plugin->run();
+
+}
+run_custom_404_pro();
